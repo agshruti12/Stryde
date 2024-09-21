f9ab823260a61839c6436d75164bdc8efa836a22
Shruti Agarwal <46203852+agshruti12@users.noreply.github.com>
2024-09-21 00:58:59 -0400

    init

---
+++ sampleSpotify/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleSpotify
+//
+//  Created by Shruti Agarwal on 9/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
